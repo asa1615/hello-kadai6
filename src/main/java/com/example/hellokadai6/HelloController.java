a3d25ceb385ef56923d3a3174222684602d1d879
asa1615 <asc.s.2127@gmail.com>
2024-03-19 05:20:22 +0900

    hello worldをレスポンスする処理の作成

--- src/main/java/com/example/hellokadai6/HelloController.java
+++ src/main/java/com/example/hellokadai6/HelloController.java
@@ -1,4 +1,18 @@
 package com.example.hellokadai6;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return"hello world";
+    }
+
+    @DeleteMapping("/hello")
+    public String deletehello(){
+        return"delete hello";
+    }
 }
