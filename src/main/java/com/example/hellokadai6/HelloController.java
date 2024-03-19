7d8f85e9b495428954156036a5a6a3dce4657f72
asa1615 <asc.s.2127@gmail.com>
2024-03-19 20:11:01 +0900

    Post処理を追加

--- src/main/java/com/example/hellokadai6/HelloController.java
+++ src/main/java/com/example/hellokadai6/HelloController.java
@@ -2,6 +2,7 @@ package com.example.hellokadai6;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +16,8 @@ public class HelloController {
     public String deletehello(){
         return"delete hello";
     }
+
+    @PostMapping("/hello")
+    public String posthello(){return "post hello";}
+
 }
